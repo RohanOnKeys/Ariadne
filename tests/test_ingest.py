fbd0fed06c1a21c852623b6d3a081a61808c0833
Rohan Pattanayak <rohanpxquantum@gmail.com>
2026-08-17 22:43:56 +0530

    test(ingest): cover bare 2-line TLE record parsing

---
+++ tests/test_ingest.py
@@ -0,0 +1,13 @@
+"""Tests for ariadne.ingest.tle."""
+
+from ariadne.ingest.tle import parse_tle_text
+
+_LINE1 = "1 25544U 98067A   24045.51782528  .00016717  00000-0  10270-3 0  9998"
+_LINE2 = "2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.49560684436460"
+
+
+def test_parses_bare_2line_record():
+    tles = parse_tle_text(f"{_LINE1}\n{_LINE2}\n")
+    assert len(tles) == 1
+    assert tles[0].norad_id == 25544
+    assert tles[0].name is None
